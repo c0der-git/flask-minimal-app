25434c6b70c875e01c0d5034a81820a4df03e43a
c0der-git <c0der-git>
2023-04-05 10:46:34 +0200

    Flask app moved to create_app()

--- flaskr/app.py
+++ flaskr/app.py
@@ -6,12 +6,23 @@ load_dotenv() #load env. variables
 from flask import Flask
 from views import view #load views/routes
 
-#debug lines
-app_name = os.environ.get('APP_NAME')
-logging.debug("Application name: "+app_name)
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(view)
 
-app = Flask(__name__)
-app.register_blueprint(view)
+    #debug lines
+    app_name = os.environ.get('APP_NAME')
+    logging.debug("Application name: "+app_name)
 
-if __name__ == '__main__':
-    app.run()
+    return app
+
+def run_app():
+    app = create_app()
+
+    #Development server run
+    if __name__ == '__main__':
+        app.run()
+
+
+
+run_app()
